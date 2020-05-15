1a590fc77f520c43aa9b7817ace0c57c12a91e09
donald1623 <donaldaudriel@gmail.com>
2020-05-15 14:55:23 +0100

    basic navigation working

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,8 @@
 <template>
-  <v-app dark>
+  <v-app light>
     <v-navigation-drawer
       v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
       fixed
-      app
     >
       <v-list>
         <v-list-item
@@ -24,61 +21,35 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
+
+
+
     <v-app-bar
-      :clipped-left="clipped"
       fixed
-      app
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      <v-app-bar-nav-icon class="hidden-lg-only"  @click.stop="drawer = !drawer" />
+      <v-btn router exact :to="'/'" text>
+        <v-icon>mdi-home</v-icon>
+        <v-toolbar-title>Dtoolz Blog</v-toolbar-title>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
+      <v-btn router exact :to="'/'" text class="mr-2 hidden-xs-only" >
+        <v-icon class="hidden-xs-only">mdi-apps</v-icon>
+         <v-toolbar-title class="hidden-xs-only">Articles</v-toolbar-title>
+      </v-btn>
+     
+      <v-btn router exact :to="'/about'" text >
+        <v-toolbar-title  class="hidden-xs-only">About Us</v-toolbar-title>
       </v-btn>
     </v-app-bar>
+
+    
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+
     <v-footer
       :fixed="fixed"
       app
@@ -92,26 +63,30 @@
 export default {
   data () {
     return {
-      clipped: false,
+     
       drawer: false,
       fixed: false,
       items: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
+          title: 'Articles',
           to: '/'
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          title: 'About-Us',
+          to: '/about'
         }
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Dtoolz Blog'
     }
   }
 }
 </script>
+
+
+<style >
+*{
+  box-sizing: border-box;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -49,8 +49,14 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      //dark: true,
       themes: {
+        light: {
+          primary: colors.purple,
+          secondary: colors.grey.darken1,
+          accent: colors.shades.black,
+          error: colors.red.accent3,
+        },
         dark: {
           primary: colors.blue.darken2,
           accent: colors.grey.darken3,
